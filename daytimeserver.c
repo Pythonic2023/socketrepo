93cb3678cdb1e7a2439fe3e193dd01966fc20eb7
Pythonic2023 <daltonfaust7295@gmail.com>
2023-03-15 18:58:22 -0400

    added daytimeserver.c and streamserver.c

---
+++ daytimeserver.c
@@ -0,0 +1,58 @@
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<sys/uio.h>		/* for iovec{} and readv/writev */
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>		/* for Unix domain sockets */
+
+// DEFINES
+#define SA struct addrinfo 
+#define MAXLINE 4096
+
+int main(int argc, char argv[]){
+    int sockfd, n;
+    char recvline[MAXLINE+1];
+    struct sockaddr_in servaddr;
+
+    if(argc != 2){
+        fprintf(stderr, "Usage: PROGRAM ARGUMENT");
+    }
+
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        fprintf(stderr, "socket error");
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(13); // daytime server
+    
+    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
+        fprintf(stderr, "inet_pton error for %s", argv[1]);
+    }
+
+    if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
+        fprintf(stderr, "connect error");
+
+        while((n = read(sockfd, recvline, MAXLINE)) > 0){
+            recvline[n] = 0; // null terminate
+            if(fputs(recvline, stdout) == EOF)
+            fprintf(stderr, "fputs error");
+        }
+        if(n < 0){
+            fprintf(stderr, "read error");
+        }
+        return 0;
+    }
+}
